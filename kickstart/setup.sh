3fe0e69102b54800bfe31cd06ba45cee4b5211e8
David Kilcy <david@kilcyconsulting.com>
2014-11-05 11:15:23 -0500

    Update setup.sh

--- kickstart/setup.sh
+++ kickstart/setup.sh
@@ -1,6 +1,8 @@
 # run as root user
 
 setenforce 0
+systemctl stop firewalld
+systemctl disable firewalld
 
 mkdir -p /home/devops/git
 chown devops.devops /home/devops/git
@@ -11,12 +13,10 @@ chown -R devops.devops /home/devops/git
 
 cd ~
 
+### Set /etc/hostname 
+
 mv /etc/hosts /etc/hosts.`date +%s`
 cp /home/devops/git/juno-saltstack/kickstart/etc/hosts /etc/hosts
-
-mkdir -p /root/staging/etc/yum.repos.d/
-mv -f /etc/yum.repos.d/* /root/staging/etc/yum.repos.d/
-cp /home/devops/git/juno-saltstack/kickstart/etc/yum.repos.d/local.repo /etc/yum.repos.d/local.repo
 
 ### Add EPEL and OpenStack repository
 
@@ -25,13 +25,6 @@ yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.
 yum install -y http://rdo.fedorapeople.org/openstack-juno/rdo-release-juno.rpm
 yum update -y
 yum upgrade -y
-
-### Setup security policies
-
-yum install -y openstack-selinux
-
-#setenforce 0
-#service iptables stop
 
 ### Create repo mirror
 
@@ -53,16 +46,37 @@ reposync -p /data/repo/centos/7/x86_64 --repoid=extras
 reposync -p /data/repo/centos/7/x86_64 --repoid=epel
 reposync -p /data/repo/centos/7/x86_64 --repoid=openstack-juno
 
+sed "s/gpgcheck=1/gpgcheck=1\nenabled=0/g" /etc/yum.repos.d/CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo
+cp /home/devops/git/juno-saltstack/kickstart/etc/yum.repos.d/local.repo /etc/yum.repos.d/local.repo
+
+yum clean all
+yum update
+
+### Setup Apache
+
 yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
  
-service ntpd start
-chkconfig ntpd on
+### Setup NTPD
+
+systemctl start ntpd
+systemctl enable ntpd
 
 ntpq -p
+
+### Setup DHCP Server
 
 yum install -y dhcp
 
 mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.`date +%s`
-ln -s /home/devops/git/juno-saltstack/kickstart/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
+cp /home/devops/git/juno-saltstack/kickstart/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
 
-service dhcpd restart
+systemctl start dhcpd
+systemctl enable dhcpd
+
+### Setup Salt Master
+
+yum install salt-master
+systemctl restart salt-master
+systemctl enable salt-master
